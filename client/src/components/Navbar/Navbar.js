3bbc7563b913c44d81397ad20b28b03ed99af575
a17alian <>
2020-12-02 23:22:33 +0100

    Working drawer menu and scrollable cards

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,25 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import ring from "../../assets/svg/ring.svg";
+import { FaHamburger, FaUserAstronaut } from "react-icons/fa";
+import { Breakpoint } from "../../styles";
 
-const Navigation = styled("nav")({
-  height: "50px",
-  display: "flex",
-  padding: "5px 20px",
-  lineHeight: "10px",
-  justifyContent: "space-between",
-  boxShadow: "0 2px 2px -2px rgba(0,0,0,.5)",
-});
+import { ImCross } from "react-icons/im";
 
-const NavIcon = styled("img")({
-  width: "30px",
-});
+const Navigation = styled.nav`
+  height: 50px;
+  display: flex;
+  padding: 5px 20px;
+  line-height: 10px;
+  justify-content: space-between;
+  padding-bottom: 30px;
+  /* box-shadow: 0 2px 2px -2px rgba(0, 0, 0, 0.5); */
+`;
+
+const DrawerMenu = styled.div`
+  height: 100%;
+  width: ${(props) => (props.open ? "250px" : "0px")};
+  ${Breakpoint.LaptopOrLarger} {
+    width: ${(props) => (props.open ? "350px" : "0px")};
+  }
+  position: fixed;
+  z-index: 1;
+  -moz-box-shadow: -3px 0 5px 0 #555;
+  -webkit-box-shadow: -3px 0 5px 0 #555;
+  box-shadow: -3px 0 5px 0 #555;
+  top: 0;
+  left: 0;
+  background-color: papayawhip;
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
+  ul {
+    display: ${(props) => (props.open ? "block" : "none")};
+  }
+  li {
+    list-style: none;
+  }
+`;
+
+const NavIcon = styled.img`
+  width: 30px;
+`;
+
+const Drawer = (props) => {
+  return <DrawerMenu open={props.open}></DrawerMenu>;
+};
+
 export const Navbar = () => {
+  const [isOpen, setOpen] = useState(false);
+  const ToggleLink = () => setOpen(!isOpen);
+
   return (
-    <Navigation>
-      <NavIcon src={ring} />
-      <h2>Hello</h2>
-      <NavIcon src={ring} />
-    </Navigation>
+    <div>
+      <Navigation>
+        <NavIcon src={ring} />
+        <h2>Learn</h2>
+        {isOpen ? (
+          <ImCross
+            onClick={ToggleLink}
+            size="30px"
+            color="#f3af2e"
+            title="exit menu button"
+            style={{ marginTop: "10px" }}
+          />
+        ) : (
+          <FaHamburger
+            onClick={ToggleLink}
+            size="30px"
+            color="#f3af2e"
+            title="menu button"
+            style={{ marginTop: "10px" }}
+          />
+        )}
+      </Navigation>
+      <Drawer open={isOpen} />
+    </div>
   );
 };
--- client/src/pages/homePage/index.js
+++ client/src/pages/homePage/index.js
@@ -7,47 +7,57 @@ const Container = styled("div")({
   flexDirection: "column",
 });
 
-const HeroContainer = styled("div")({
-  minHeight: "30vh",
-  backgroundColor: "green",
-  [Breakpoint.Desktop]: {
-    minHeight: "50vh",
-    backgroundColor: "yellow",
-  },
-});
-const CategoryContainer = styled("div")({
-  margin: "5px 20px",
-  display: "grid",
-  gridAutoRows: "200px 200px",
-  gridGap: "10px",
-  gridTemplateColumns: "repeat(auto-fit, minmax(200px,1fr))",
-  gridAutoFlow: "dense",
-  [Breakpoint.LaptopOrLarger]: {
-    gridAutoRows: "300px 300px",
-    gridGap: "20px",
-    gridTemplateColumns: "repeat(auto-fit, minmax(300px,1fr))",
-  },
-  [Breakpoint.BigScreenOrLarger]: {
-    width: "70%",
-    margin: "0 auto",
-  },
-});
+const HeroContainer = styled.div`
+  min-height: 30vh;
+  background-color: #91697e;
+  ${Breakpoint.BigScreenOrLarger} {
+    min-height: 50vh;
+    background-color: yellow;
+  }
+`;
+const CategoryContainer = styled.div`
+  margin: 5px 20px;
+  display: grid;
+  grid-auto-rows: 200px 200px;
+  grid-gap: 10px;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-auto-flow: dense;
+  ${Breakpoint.LaptopOrLarger} {
+    grid-auto-rows: 300px 300px;
+    grid-gap: 20px;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  }
+  ${Breakpoint.BigScreenOrLarger} {
+    width: 70%;
+    margin: 0 auto;
+  }
+`;
 const HomePage = () => {
   const categories = [
-    { title: "Lion ", color: "#f4b02d" },
-    { title: "Hippo", color: "#8f647b" },
-    { title: "Rabbit", color: "#6db996" },
-    { title: "Girafe", color: "#e98159" },
-    { title: "Tiger", color: "#efb84d" },
-    { title: "Bear", color: "#6cba96" },
+    {
+      title: "Räkna ",
+      color: "#f4b02d",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mauris justo, cursus in nulla sit amet, sodales interdum dolor. Aliquam dapibus placerat tellu.",
+    },
+    { title: "Skriva", color: "#8f647b" },
+    { title: "Engelska", color: "#6db996" },
+    { title: "Glosor", color: "#e98159" },
+    { title: "Alfabetet", color: "#efb84d" },
+    { title: "Klockan", color: "#6cba96" },
   ];
 
   return (
     <Container>
-      <HeroContainer></HeroContainer>
       <CategoryContainer>
         {categories.map((category, i) => {
-          return <Card title={category.title} cardColor={"blue"} key={i} />;
+          return (
+            <Card
+              title={category.title}
+              cardColor={category.color}
+              text={category.description}
+            />
+          );
         })}
       </CategoryContainer>
     </Container>
